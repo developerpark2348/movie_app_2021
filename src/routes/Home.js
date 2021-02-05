cf48744ddb5b06754ec33994ae8adf29d3ec57a8
developerpark2348 <developerpark2348@gmail.com>
2021-02-05 15:22:04 +0900

    components, routes폴더로 정리
    폴더, 파일 정리

---
+++ src/routes/Home.js
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Movie from "../components/Movie";
+import "./Home.css";
+
+function Home() {
+
+    let [로딩, 로딩변경] = useState(true)
+    let [영화, 영화변경] = useState([])
+    
+    let getMovies = async () => {
+        const movies = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json')
+        영화변경(movies.data.data.movies);
+        로딩변경(false);
+    };
+
+    useEffect(()=>{
+        getMovies();
+    }, []);
+
+    return(
+        <section className="contatiner">
+            {로딩
+            ? 
+            <div className="loader">
+                <span className="loader__text">Loading...</span>
+            </div>
+            : 
+            <div className="movies">
+            {영화.map(영화 => {
+                return (
+                    <Movie
+                        key={영화.id}
+                        id={영화.id} 
+                        year={영화.year} 
+                        title={영화.title} 
+                        summary={영화.summary} 
+                        poster={영화.medium_cover_image}
+                        genres={영화.genres}
+                    />
+                );
+            }) }
+            </div>
+            }
+        </section>
+    );
+
+}
+
+export default Home;
