9b9b12f0139305d8ae588486995b3c35815578a3
developerpark2348 <developerpark2348@gmail.com>
2021-02-05 17:22:03 +0900

    React내 새로고침없이 페이지 이동, 스테이트 전달하기
    react-router-dom에서의 Link를 사용한다.
    ex) <Link to="경로"> </Link>
    
    * Link를 사용하기 위해서는 Link를 사용하는 컴포넌트는 <HashRouter>내에 있어야한다.
    
    각 컴포넌트들은 리액트에서 받은 props를 가지고있다. 이걸 이용하여 A를 클릭시 A안의 props를 가져와 A의 정보를 가져올수있다.
    
    ex)
    Link to= {{
    pathname: "주소",
    state: {
    year: props.~
    ~: props.~
    },
    }}
    >
    등과 같이 props내 정보들을 보내줄수있음.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './Movie.css';
 
 function Movie(props){
     return(
-    <div className="movie">
-        <img src={props.poster} alt={props.title} title={props.title} />
-        <div className="movie__data">
-            <h3 className="movie__title">{props.title}</h3>
-            <h5 className="movie__year">{props.year}</h5>
-            <ul className="movie__genres">
-                {props.genres.map((genre, index) => {
-                    return(
-                    <li key={index} className="genres__genre">{genre}</li>
-                    )
-            })}
-            </ul>
-            <p className="movie__summary">{props.summary.slice(0,180)}...</p>
+    <Link 
+        to={{
+            pathname: "/movie-detail",
+            state: {
+                year:props.year,
+                title:props.title,
+                summary:props.summary,
+                poster:props.poster,
+                genres:props.genres
+            },
+        }}
+    >
+        <div className="movie">
+            <img src={props.poster} alt={props.title} title={props.title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{props.title}</h3>
+                <h5 className="movie__year">{props.year}</h5>
+                <ul className="movie__genres">
+                    {props.genres.map((genre, index) => {
+                        return(
+                        <li key={index} className="genres__genre">{genre}</li>
+                        )
+                })}
+                </ul>
+                <p className="movie__summary">{props.summary.slice(0,180)}...</p>
+            </div>
         </div>
-    </div>
+    </Link>
     )
 }
 
